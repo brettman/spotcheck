ab1942f93b04258672a7af1858d15d294d29d57f
Brett Hardman <bretthardman@devies.se>
2023-03-17 22:38:13 +0100

    initial commit

--- spotcheck.service/Class1.cs
+++ spotcheck.service/Class1.cs
@@ -1,5 +1,19 @@
-﻿namespace spotcheck.service;
-public class Class1
-{
+﻿using Microsoft.Extensions.Logging;
 
+namespace spotcheck.service;
+public class SpotifyLoginClient 
+{
+    private readonly HttpClient _httpClient;
+    private readonly ILogger<SpotifyLoginClient> _logger;
+
+    public SpotifyLoginClient(HttpClient httpClient, ILogger<SpotifyLoginClient> logger )
+    {
+        _httpClient = httpClient;
+        _logger = logger;
+    }
+
+    public string GetLoginToken()
+    {
+        
+    }
 }
